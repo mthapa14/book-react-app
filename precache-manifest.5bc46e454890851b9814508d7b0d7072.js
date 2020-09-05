02aeda9a2f7be144b8c9f0da7853805619fa698c
Mohan Thapa <mohan.thapa5767@gmail.com>
2020-09-05 20:37:47 +1000

    Updates

---
+++ precache-manifest.5bc46e454890851b9814508d7b0d7072.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "670208efd1b033cf284782a9134bff3e",
+    "url": "/book-react-app/index.html"
+  },
+  {
+    "revision": "d6b8fa265a67c7fbeb3f",
+    "url": "/book-react-app/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "688aef097017a95b0913",
+    "url": "/book-react-app/static/css/main.d1dc0c90.chunk.css"
+  },
+  {
+    "revision": "d6b8fa265a67c7fbeb3f",
+    "url": "/book-react-app/static/js/2.a6c31650.chunk.js"
+  },
+  {
+    "revision": "d3031346469a8a2ef527f1ad705ea3b7",
+    "url": "/book-react-app/static/js/2.a6c31650.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "688aef097017a95b0913",
+    "url": "/book-react-app/static/js/main.a2d392a7.chunk.js"
+  },
+  {
+    "revision": "bfa0c5e1ae3e3eb86332",
+    "url": "/book-react-app/static/js/runtime-main.311a4c81.js"
+  },
+  {
+    "revision": "ecc194d5842e2a5504856033a12f50ae",
+    "url": "/book-react-app/static/media/avenger.ecc194d5.jpg"
+  },
+  {
+    "revision": "e72d5cac6224ec0079405108924866a8",
+    "url": "/book-react-app/static/media/batman.e72d5cac.jpg"
+  },
+  {
+    "revision": "1fe344ea12f008e08a672d0b2a30f12d",
+    "url": "/book-react-app/static/media/blackhammer.1fe344ea.jpg"
+  },
+  {
+    "revision": "9e76b6524de78a1712500845e68a5fc2",
+    "url": "/book-react-app/static/media/harrypotter.9e76b652.jpg"
+  },
+  {
+    "revision": "0b9324c6747f198fa460485af3b5f7c4",
+    "url": "/book-react-app/static/media/spiderman.0b9324c6.jpg"
+  },
+  {
+    "revision": "5827aaf997901d82ab3ea3e070f111e6",
+    "url": "/book-react-app/static/media/venom.5827aaf9.jpg"
+  },
+  {
+    "revision": "d1feba84d39da6014fd316effad3cc9d",
+    "url": "/book-react-app/static/media/wasp.d1feba84.jpg"
+  }
+]);
